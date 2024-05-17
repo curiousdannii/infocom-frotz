464ecd286773526aff0b8fdf073d6585d099b178
Dannii Willis <curiousdannii@gmail.com>
2024-05-17 16:56:37 +1000

    Support higher pixel densities. Pretty hacky though

--- src/frotz-library.js
+++ src/frotz-library.js
@@ -2,6 +2,47 @@
 // For replacing some functions
 
 const FROTZ_JS = {
+    // Handle high pixel densities by faking it...
+    emscripten_get_element_css_size(target, width, height) {
+        target = findEventTarget(target)
+        if (!target) {
+            return -4
+        }
+        if (typeof target.requestedheight === 'number') {
+            HEAPF64[((width)>>3)] = target.requestedwidth
+            HEAPF64[((height)>>3)] = target.requestedheight
+        }
+        else {
+            var rect = getBoundingClientRect(target)
+            HEAPF64[((width)>>3)] = rect.width
+            HEAPF64[((height)>>3)] = rect.height
+        }
+        return 0
+    },
+
+    emscripten_set_canvas_element_size(target, width, height){
+        var canvas = findCanvasEventTarget(target)
+        if (!canvas) {
+            return -4
+        }
+        canvas.width = width
+        canvas.height = height
+        const screen_width = screen.width
+        if (width > screen_width) {
+            canvas.requestedwidth = width
+            canvas.requestedheight = height
+            canvas.style.width = `${screen_width}px`
+            canvas.style.height = `${screen_width * (height / width)}px`
+        }
+        else {
+            canvas.requestedwidth = null
+            canvas.requestedheight = null
+            canvas.style.width = ''
+            canvas.style.height = ''
+        }
+        return 0
+    },
+
     // emscripten_set_main_loop_timing just errors because it's called at the wrong time
     emscripten_set_main_loop_timing() {},
 
