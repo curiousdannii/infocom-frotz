02581f7a5605302b911da8be7e75a897f05675a2
Dannii Willis <curiousdannii@gmail.com>
2024-05-18 16:18:59 +1000

    Prevent iOS from zooming in when focusing input

--- src/preamble.js
+++ src/preamble.js
@@ -6,6 +6,13 @@ Module.arguments = ['/' + files[0], '/' + files[1]]
 Module.canvas = document.getElementById('canvas')
 
 Module.preRun = () => {
+    // Prevent iOS from zooming in when focusing input, but allow Android to still pinch zoom
+    // As they handle the maximum-scale viewport meta option differently, we will conditionally add it only in iOS
+    // Idea from https://stackoverflow.com/a/62750441/2854284
+    if (/iPhone OS/i.test(navigator.userAgent)) {
+        document.head.querySelector('meta[name="viewport"]').content = 'width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1'
+    }
+
     // Load storyfile and blorb
     FS.createPreloadedFile('/', files[0], files[0], true, false)
     FS.createPreloadedFile('/', files[1], files[1], true, false)
