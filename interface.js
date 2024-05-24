a612892663d774925f039cb3800ab164cfaa0149
Dannii Willis <curiousdannii@gmail.com>
2024-05-24 11:26:35 +1000

    Add mode buttons to Arthur

--- interface.js
+++ interface.js
@@ -3,6 +3,22 @@
 const canvas_elem = document.getElementById('canvas')
 const textinput_elem = document.getElementById('textinput')
 
+// Prevent iOS from zooming in when focusing input, but allow Android to still pinch zoom
+// As they handle the maximum-scale viewport meta option differently, we will conditionally add it only in iOS
+// Idea from https://stackoverflow.com/a/62750441/2854284
+if (/iPhone OS/i.test(navigator.userAgent)) {
+    document.head.querySelector('meta[name="viewport"]').content = 'width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1'
+}
+
+visualViewport.addEventListener('resize', () => {
+    const height = visualViewport.height
+    document.documentElement.style.height = `${height}px`
+
+    // Safari might have scrolled weirdly, so try to put it right
+    window.scrollTo(0, 0)
+    setTimeout(() => window.scrollTo(0, 0), 500)
+})
+
 // Mobile input event handlers
 canvas_elem.addEventListener('touchstart', ev => {
     textinput_elem.focus()
@@ -40,32 +56,12 @@ textinput_elem.addEventListener('input', ev => {
 })
 textinput_elem.addEventListener('keydown', ev => {
     if (ev.which === 8) {
-        const options = {
-            code: 'Backspace',
-            key: 'Backspace',
-            keyCode: 8,
-            which: 8,
-        }
-        window.dispatchEvent(new KeyboardEvent('keydown', options))
-        window.dispatchEvent(new KeyboardEvent('keyup', options))
-        ev.preventDefault()
-        ev.stopPropagation()
+        send_fake_keydown(8)
     }
     if (ev.which === 13) {
-        const options = {
-            charCode: 13,
-            code: 'Enter',
-            key: 'Enter',
-            keyCode: 13,
-            which: 13,
-        }
-        window.dispatchEvent(new KeyboardEvent('keydown', options))
-        window.dispatchEvent(new KeyboardEvent('keypress', options))
-        window.dispatchEvent(new KeyboardEvent('keyup', options))
-        ev.preventDefault()
-        ev.stopPropagation()
+        send_fake_keydown(13)
     }
-    if (ev.which === 229) {
+    if (ev.which === 8 || ev.which === 13 || ev.which === 229) {
         ev.preventDefault()
         ev.stopPropagation()
     }
@@ -76,3 +72,41 @@ textinput_elem.addEventListener('keyup', ev => {
         ev.stopPropagation()
     }
 })
+
+// Arthur buttons
+const arthurmodes = document.getElementById('arthurmodes')
+if (arthurmodes) {
+    arthurmodes.addEventListener('click', ev => {
+        let target = ev.target
+        if (target.tagName === 'SPAN') {
+            target = target.parentNode
+        }
+        if (target.tagName === 'P') {
+            send_fake_keydown(parseInt(target.dataset.keycode, 10))
+        }
+        textinput_elem.focus()
+    })
+}
+
+// Send a fake keydown/keyup
+const named_codes = {
+    8: 'Backspace',
+    13: 'Enter',
+}
+function send_fake_keydown(code) {
+    const name = named_codes[code] || `F${code - 111}`
+    const data = {
+        code: name,
+        key: name,
+        keyCode: code,
+        which: code,
+    }
+    if (code === 13) {
+        data.charCode = 13
+    }
+    window.dispatchEvent(new KeyboardEvent('keydown', data))
+    if (code === 13) {
+        window.dispatchEvent(new KeyboardEvent('keypress', data))
+    }
+    window.dispatchEvent(new KeyboardEvent('keyup', data))
+}
